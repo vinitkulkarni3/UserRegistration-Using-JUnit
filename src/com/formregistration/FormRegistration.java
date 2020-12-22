e0551a5d2a4a736f531d382804ce2746c89a8dc6
vinitkulkarni3 <vinitkulkarni4321@gmail.com>
2020-12-22 23:00:46 +0530

    Added: firstname testcase

--- src/com/formregistration/FormRegistration.java
+++ src/com/formregistration/FormRegistration.java
@@ -1,5 +1,24 @@
 package com.formregistration;
 
+import java.util.regex.Pattern;
+
 public class FormRegistration 
 {   
+    //Constants
+    final String FIRSTNAMEPATTERN =  "^[A-Z]{1}[a-z]{2,}$";
+    
+    //First Name validation
+    public boolean firstNameValidation(String firstName)
+    {
+        boolean firstNameFlag = false;
+        if(Pattern.matches(FIRSTNAMEPATTERN, firstName))
+        {
+            firstNameFlag = true;
+        }
+        else
+        {
+            firstNameFlag = false;
+        }
+        return firstNameFlag;
+    }
 }
--- test/com/formregistration/TestFormRegistration.java
+++ test/com/formregistration/TestFormRegistration.java
@@ -1,5 +1,25 @@
 package com.formregistration;
 
+import static junit.framework.Assert.assertEquals;
+import org.junit.Test;
+
 public class TestFormRegistration
-{    
+{
+    FormRegistration formObject = new FormRegistration();
+    
+    //first name validation test case
+    @Test
+    public void getFirstName_whenProper_shouldReturnTrue() 
+    {    
+        boolean result = formObject.firstNameValidation("Vinit"); //starts with capital letter and minimum 3 letters
+        assertEquals(true, result);
+        //assertTrue(result);
+    }
+    @Test
+    public void getFirstName_whenNotProper_shouldReturnFalse() 
+    {    
+        boolean result = formObject.firstNameValidation("Vi"); //Starts with capital letter and only 2 character 
+        assertEquals(true, result);
+        //assertFalse(result);
+    }
 }
